d5b52796adbf85592c548e91a9f19a54883517d8
integi <integi@outlook.com>
2021-09-22 10:38:58 +0200

    Strona test

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Witaj blogu",
+        "excerpt":"Witamy na blogu Integi!  ","categories": ["Ogólne"],
+        "tags": ["Cześć"],
+        "url": "/og%C3%B3lne/welcome/",
+        "teaser": null
+      },{
+        "title": "Żongler Comarch ERP XL",
+        "excerpt":"Żongler jest narzędziem przeznaczonym do szybkiego przełączania się pomiędzy różnymi wersjami Comarch ERP XL zainstalowanymi na tym samym komputerze. Aplikacja jest przeznaczoa dla pracowników firm wdrożeniowych którzy wykonują kilka razy dziennie ręczną operację zmiany wersji systemu. Do prawidłowego przestawienia się na inną wersję wymagane jest wykonania kilku niezbędnych kroków -...","categories": ["Comarch ERP XL"],
+        "tags": ["Comarch ERP XL","GitHub"],
+        "url": "/comarch%20erp%20xl/juggler/",
+        "teaser": null
+      }]
